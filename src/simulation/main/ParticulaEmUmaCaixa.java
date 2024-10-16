a103700211624a6bc117d9ba4b7639511ed01f30
Jônatas da Silva Gonçalves <jngoncalves721@gmail.com>
2024-10-15 21:23:21 -0300

    add inimated test code

---
+++ src/simulation/main/ParticulaEmUmaCaixa.java
@@ -0,0 +1,10 @@
+package simulation.main;
+
+import javax.swing.SwingUtilities;
+import simulation.main.teste.AnimatedWaveFunction;
+
+public class ParticulaEmUmaCaixa {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(AnimatedWaveFunction::new);
+    }
+}
